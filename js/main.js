2008fa34b00ea63f100ef1cb7d6c493a31a12dce
Schettino <edu.schettino98@gmail.com>
2022-12-02 19:03:35 -0300

    se agrega funcion para buscar pelicula

--- js/main.js
+++ js/main.js
@@ -2,76 +2,84 @@ let post = []
 
 const peliculasContainer = document.querySelector(".peliculas")
 
+const input = document.querySelector("#buscador")
+const buscador = () => {
+    input.addEventListener("keyup", (e) => {
+        let datos = document.querySelector("#buscador").value
+        console.log(datos)
+        const Titulospelis = document.querySelectorAll("#titulo")
+        Titulospelis.forEach((e)=>{
+            const div= e.parentElement.parentElement
+            if (e.textContent.toLowerCase().includes(datos.toLowerCase())) {
+                div.classList.remove("hidden")
+            } else {
+                div.classList.add("hidden")
+            }
+        })
+        // let busqueda = post.filter(titulo => datos = titulo.title)
+        // console.log(busqueda)
+    })
+
+}
+
 
 const renderizarPeliculas = () => {
     post.forEach((e) => {
-        console.log(e)
         peliculasContainer.innerHTML += `<div class="post">
-                                            <img src="${e.image}">
-                                            <div class="info">
-                                                <h3>${e.title}</h3>
-                                                <h5>${e.year}</h5>
-                                                <button>
-                                                    <span class="button_top"> Ver Horarios
-                                                    </span>
-                                                </button>
-                                            </div>
-                                        </div>`
+        <img src="${e.image}">
+        <div class="info">
+            <h3 id="titulo">${e.title}</h3>
+            <h5>${e.year}</h5>
+            <button id="btnHorarios">
+                <span class="button_top"> Ver Horarios
+                </span>
+            </button>
+            <div class="infoExtra">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta perspiciatis atque repudiandae
+                deleniti minus voluptas quo suscipit debitis ipsum molestias facilis aperiam delectus, quia lore</p>
+            </div>
+        </div>
+    </div>`
+    })
+    mostrarHorarios()
+}
+
+
+const mostrarHorarios = () => {
+    const btnHorarios = document.querySelectorAll("#btnHorarios")
+    btnHorarios.forEach((boton) => {
+        boton.addEventListener('click', () => {
+            const containerInfoExtra = boton.parentElement
+            console.log(containerInfoExtra)
+            const infoExtra = containerInfoExtra.querySelector(".infoExtra")
+            if (containerInfoExtra.classList.contains("ver")) {
+                containerInfoExtra.classList.remove("ver")
+                infoExtra.classList.remove("verExtra")
+            }
+            else {
+                containerInfoExtra.classList.add("ver")
+                infoExtra.classList.add("verExtra")
+            }
+        })
     })
 }
 
 
-let containerPost = document.querySelector(".post")
-let containerHorarios = document.querySelector(".horarios")
-
-containerPost.max = containerPost.offsetHeight + "px"
-containerHorarios.style.display = "none"
-containerPost.min = containerPost.offsetHeight + "px"
-containerPost.style.height = containerPost.min;
-containerPost.style.transition = "height";
-
-const toggle = () => {
-    if (containerPost.style.height != containerPost.min && containerPost.style.height != '') {
-        containerPost.style.height = containerPost.min;
-    } else {
-        containerPost.style.height = containerPost.max;
-        containerHorarios.style.display = "inline";
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '72949765c0msh3b252535d3e8281p14341bjsn3c3c6380cff5',
+        'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
     }
-}
+};
+
+fetch('https://imdb-top-100-movies.p.rapidapi.com/premiummovies', options)
+    .then(response => response.json())
+    .then(data => {
+        post = data
+        renderizarPeliculas()
+    })
+    .catch(err => console.error(err));
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         'X-RapidAPI-Key': '72949765c0msh3b252535d3e8281p14341bjsn3c3c6380cff5',
-//         'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
-//     }
-// };
-
-// fetch('https://imdb-top-100-movies.p.rapidapi.com/premiummovies', options)
-//     .then(response => response.json())
-//     .then(data => {
-//         post = data
-//         renderizarPeliculas()
-//     })
-//     .catch(err => console.error(err));
-
+buscador()
